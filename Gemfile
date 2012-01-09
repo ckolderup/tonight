dddcf9284e0d8939dba95ba725fc29f06972794f
Casey Kolderup <casey@kolderup.org>
2012-01-09 00:27:27 -0500

    boom

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source :gemcutter
+gem 'sinatra', '1.2.6'
+gem 'data_mapper', '1.2.0'
+gem 'dm-sqlite-adapter', '1.2.0'
+gem 'dm-postgres-adapter'
+gem 'haml', '3.0.25'
---
+++ coalhouse.rb
@@ -0,0 +1,48 @@
+require 'rubygems'
+require 'sinatra'
+require 'data_mapper'
+require 'haml'
+require 'sass'
+
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite://#{Dir.pwd}/coalhouse.db")
+
+class Attending
+  include DataMapper::Resource
+  property :id,     Serial
+  property :name,   Text
+  property :timestamp, DateTime
+end
+
+DataMapper.auto_upgrade!
+
+class Coalhouse < Sinatra::Application
+  get '/' do
+    t = Date.today
+    stale = Attending.all(:timestamp.lt => DateTime.new(t.year, t.month, t.day))
+    stale.each { |a| a.destroy }
+   
+    @attending = Attending.all
+    haml :coalhouse
+  end
+
+  post '/add' do
+    name = params[:name]
+    name = 'Lazy Mystery Guest' if name.length == 0
+    time = DateTime.now
+
+    Attending.create(:name => name, :timestamp => time)
+
+    #flash[:notice] = "Added #{name}"
+    redirect '/', 303
+  end
+
+  post '/delete' do
+    id = params[:id]
+
+    victim = Attending.get(id)
+    victim.destroy
+   
+    #flash[:notice] = "Removed #{victim.name}"
+    redirect '/', 303
+  end
+end
