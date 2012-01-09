c88e695d493effaf1aef4937789e03976d1a5ee1
Casey Kolderup <casey@kolderup.org>
2012-01-09 01:01:54 -0500

    broken on heroku

--- coalhouse.rb
+++ coalhouse.rb
@@ -28,7 +28,7 @@ class Coalhouse < Sinatra::Application
   post '/add' do
     name = params[:name]
     name = 'Lazy Mystery Guest' if name.length == 0
-    time = DateTime.zone.now
+    time = DateTime.now
 
     Attending.create(:name => name, :timestamp => time)
 
