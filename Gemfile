a25bb01a0251c8984809b2121d176e1034fc8d79
Casey Kolderup <casey@kolderup.org>
2012-02-02 23:24:27 -0500

    basic support for dividing things up by subdomain

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 source :gemcutter
-gem 'sinatra', '1.2.6'
+gem 'rack', '~>1.3.6'
+gem 'sinatra'
 gem 'data_mapper', '1.2.0'
 gem 'dm-sqlite-adapter', '1.2.0'
 gem 'dm-postgres-adapter'
 gem 'haml', '3.0.25'
+gem 'sinatra-subdomain'
--- whos-in.rb
+++ whos-in.rb
@@ -3,12 +3,14 @@ require 'sinatra'
 require 'data_mapper'
 require 'haml'
 require 'sass'
+require 'sinatra/subdomain'
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite://#{Dir.pwd}/whos-in.db")
 
 class Attending
   include DataMapper::Resource
   property :id,     Serial
+  property :subdomain, Text
   property :name,   Text
   property :timestamp, DateTime
 end
@@ -17,36 +19,45 @@ DataMapper.auto_upgrade!
 
 class WhosIn < Sinatra::Application
   enable :sessions
+  register Sinatra::Subdomain
   
+  subdomain do
+    get '/' do
+      t = Date.today
+      stale = Attending.all(:timestamp.lt => DateTime.new(t.year, t.month, t.day))
+      stale.each { |a| a.destroy }
+      
+      @attending = Attending.all :subdomain => subdomain, :order => :timestamp.desc
+      @added_id = session.delete :added_id
+      
+      haml :index
+    end
+    
+    post '/add' do
+      name = params[:name]
+      name = 'Lazy Mystery Guest' if name.length == 0
+      time = DateTime.now
+
+      attendee = Attending.create(:name => name, :subdomain => subdomain, 
+                                  :timestamp => time)
+
+      session[:added_id] = attendee.id
+
+      redirect '/', 303
+    end
+
+    post '/delete' do
+      id = params[:id]
+
+      victim = Attending.get(id)
+      victim.destroy
+     
+      redirect '/', 303
+    end
+  end
+
   get '/' do
-    t = Date.today
-    stale = Attending.all(:timestamp.lt => DateTime.new(t.year, t.month, t.day))
-    stale.each { |a| a.destroy }
-    
-    @attending = Attending.all :order => :timestamp.desc
-    @added_id = session.delete :added_id
-    
-    haml :index
+    "insert info page here"
   end
 
-  post '/add' do
-    name = params[:name]
-    name = 'Lazy Mystery Guest' if name.length == 0
-    time = DateTime.now
-
-    attendee = Attending.create(:name => name, :timestamp => time)
-
-    session[:added_id] = attendee.id
-
-    redirect '/', 303
-  end
-
-  post '/delete' do
-    id = params[:id]
-
-    victim = Attending.get(id)
-    victim.destroy
-   
-    redirect '/', 303
-  end
 end
