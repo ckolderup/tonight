714c92e29a77fa6200adeb13dd23a36113a5dd5c
Casey Kolderup <casey@kolderup.org>
2012-02-03 00:09:50 -0500

    unstyled info page

--- whos-in.rb
+++ whos-in.rb
@@ -4,6 +4,7 @@ require 'data_mapper'
 require 'haml'
 require 'sass'
 require 'sinatra/subdomain'
+require 'random-word'
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite://#{Dir.pwd}/whos-in.db")
 
@@ -23,13 +24,19 @@ class WhosIn < Sinatra::Application
   
   subdomain do
     get '/' do
+
+      if subdomain == "www"
+        info_page
+        exit
+      end
+
       t = Date.today
       stale = Attending.all(:timestamp.lt => DateTime.new(t.year, t.month, t.day))
       stale.each { |a| a.destroy }
-      
+
       @attending = Attending.all :subdomain => subdomain, :order => :timestamp.desc
       @added_id = session.delete :added_id
-      
+
       haml :index
     end
     
@@ -51,13 +58,24 @@ class WhosIn < Sinatra::Application
 
       victim = Attending.get(id)
       victim.destroy
-     
+
       redirect '/', 303
     end
   end
 
   get '/' do
-    "insert info page here"
+    info_page
   end
 
+  def info_page
+    haml :info, :locals => {:rando => unoccupied_word}
+  end
+
+  def unoccupied_word
+    begin
+      rando = RandomWord.nouns.first.gsub('_','-')
+      count = Attending.count(:subdomain => rando)
+    end while count != 0
+    rando
+  end
 end
