4cf180b014390a22ea08370a8fa3cf8ab2e82809
John Holdun <john@johnholdun.com>
2012-03-14 15:01:49 -0400

    Remove all the jQuery stuff in favor of some 2006 javascript

--- public/tonight.js
+++ public/tonight.js
@@ -1,3 +1,18 @@
-$(function() {
-  $('.timestamp').timezone();
-});
+(function(){
+  var
+    offset = (new Date()).getTimezoneOffset() * -60000,
+    timestamps = document.getElementsByClassName("timestamp"),
+    i,
+    timestamp,
+    date,
+    label
+  
+  for (i = 0; i < timestamps.length; i++) {
+    timestamp = timestamps[i]
+    date = new Date(parseInt(timestamp.attributes["data-unixtime"].value, 10) * 1000 + offset)
+    label = (date.getHours() > 12 ? date.getHours() - 12 : date.getHours())
+    label += ":" + date.getMinutes()
+    label += (date.getHours() >= 12 ? "PM" : "AM")
+    timestamp.innerHTML = label
+  }
+})()
