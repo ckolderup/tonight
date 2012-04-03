164451edf81ca50f955e7c2214f22a4efcc66fa7
Casey Kolderup <casey@kolderup.org>
2012-04-02 20:36:28 -0400

    clean up formatting

--- tonight.rb
+++ tonight.rb
@@ -31,50 +31,51 @@ class Tonight < Sinatra::Application
   register Sinatra::Subdomain
   set :haml, { :format => :html5, :ugly => true }
   set :sass, { :style => :compressed }
-  
+
   before do
     @app_env = (request.host =~ /\.pm$/) ? :production : :development
   end
-  
+
   subdomain do
     get '/' do
       if subdomain == "www"
         info_page
         exit
       end
-      
+
       reset_page subdomain
-      
+
       fetch_resources subdomain
-      
+
       @added_id = session.delete :added_id
 
       haml :index
     end
-    
+
     post '/' do
       fetch_resources subdomain
-      
+
       @event.title = params[:title]
       @event.save
-      
+
       redirect to('/')
     end
-    
+
     get '/copy' do
       reset_page subdomain
-      
+
       fetch_resources subdomain
-      
+
       @output = {}
-      
+
       [:in, :out].each do |group|
-        @output[group] = (["#{@responders[group].size} #{group}:"] + @responders[group].map(&:name)).join("\n")
+        @output[group] = (["#{@responders[group].size} #{group}:"] + 
+                          @responders[group].map(&:name)).join("\n")
       end
-      
+
       haml :copy
     end
-        
+
     post '/add' do
       name = params[:name]
       name = 'Lazy Mystery Guest' if name.length == 0
@@ -112,13 +113,13 @@ class Tonight < Sinatra::Application
     @info = true
     haml :info, :locals => {:rando => unoccupied_word}
   end
-  
+
   def fetch_resources subdomain
     @event = Event.first :subdomain => subdomain
     @event ||= Event.create :subdomain => subdomain, :title => subdomain
 
     @responders = { :in => [], :out => [] }
-    
+
     Attending.all(:subdomain => subdomain, :order => :timestamp.desc).each do |responder|
       @responders[responder.declined ? :out : :in] << responder
     end
