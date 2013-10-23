a2b5a33c3094dc5c102ce9bfdbce1504f843769d
Aaron N. Tubbs <atubbs@server.fake>
2013-10-23 17:36:48 -0400

    From each according to their (scope) ability, to each according to their (scope) need. Addresses @lcapaldo's concerns, though my inexperience with JavaScript may have made this a severe or poor-style adaptation of the advice? In specific JavaScript's semicolon insertion rules have made me opt to include them to terminate statements when not (always) strictly necessary.

--- public/tonight.js
+++ public/tonight.js
@@ -1,65 +1,59 @@
 (function(){
-  var
-    offset = (new Date()).getTimezoneOffset() / -60 * 1000,
-    timestamps = document.getElementsByClassName("timestamp"),
-    i,
-    timestamp,
-    date,
-    label,
-    title = document.querySelector("h2"),
-    copyLink = document.querySelector("a[href='/copy']"),
-    inputTitle = document.getElementById("input-title"),
-    inputIdx,
-    inputPrefix,
-    localizedTimestamp,
-    selectOutField
 
-  localizedTimestamp = function(o) {
-    date = new Date(parseInt(o.attributes["data-unixtime"].value, 10) * 1000)
-    label = (date.getHours() > 12 ? date.getHours() - 12 : date.getHours())
-    label += ":" + (date.getMinutes() < 10 ? "0" : "") + date.getMinutes()
-    label += (date.getHours() >= 12 ? "PM" : "AM")
-    return label
-  }
-  
-  for (i = 0; i < timestamps.length; i++) {
-    timestamp = timestamps[i]
-    timestamp.innerHTML = localizedTimestamp(timestamp)
-  }
+  (function(){
+    var localizedTimestamp = function(o) {
+      var date = new Date(parseInt(o.attributes["data-unixtime"].value, 10) * 1000);
+      var label = (date.getHours() > 12 ? date.getHours() - 12 : date.getHours());
+      label += ":" + (date.getMinutes() < 10 ? "0" : "") + date.getMinutes();
+      label += (date.getHours() >= 12 ? "PM" : "AM");
+      return label;
+    };
+    
+    (function(){
+      var timestamps = document.getElementsByClassName("timestamp");
+      for (var i = 0; i < timestamps.length; i++) {
+        var timestamp = timestamps[i];
+        timestamp.innerHTML = localizedTimestamp(timestamp);
+      }
+    }());
 
-  inputIdx = inputTitle.title.lastIndexOf(" ") + 1;
-  if (-1 != inputIdx) {
-    inputPrefix = inputTitle.title.substr(0, inputIdx);
-    inputTitle.title = inputPrefix + localizedTimestamp(inputTitle)
-  }
-  
-  selectOutField = function(e) {
-    if (e && e.target && e.target.nodeName == "H2") return;
-    var
-      side = (e && e.target && e.target.className) || "out",
-      responders = document.querySelector(".responders." + side)
+    (function(){
+      var inputTitle = document.getElementById("input-title");
+      var inputIdx = inputTitle.title.lastIndexOf(" ") + 1;
+      if (-1 != inputIdx) {
+        var inputPrefix = inputTitle.title.substr(0, inputIdx);
+        inputTitle.title = inputPrefix + localizedTimestamp(inputTitle);
+      }
+    }());
+  }());
 
-    if (copyLink) copyLink.attributes.href.value = "/copy" + (side === "out" ? "#out" : "")
-    ;(responders.nodeName === "TEXTAREA" ? responders : document.querySelector("." + side + " form.add input"))
-      .select()
-  }
-  
-  if (location.hash === "#out") selectOutField()
-
-  title && (title.onclick = selectOutField)
-
-  
-  resetOutButtonState = function() {
-    var out = document.querySelector(".out form.add button")
-    if (this.value == "") {
-      out.setAttribute('disabled', 'disabled')
-    } else {
-      out.removeAttribute('disabled')
+  (function(){
+    var copyLink = document.querySelector("a[href='/copy']");
+    var selectOutField = function(e) {
+      if (e && e.target && e.target.nodeName == "H2") return;
+      var side = (e && e.target && e.target.className) || "out",
+        responders = document.querySelector(".responders." + side);
+      if (copyLink) copyLink.attributes.href.value = "/copy" + (side === "out" ? "#out" : "");
+      (responders.nodeName === "TEXTAREA" ? responders : document.querySelector("." + side + " form.add input")).select();
     }
-  }
-  
-  var name = document.querySelector(".out form.add input#name")
+    if (location.hash === "#out") selectOutField();
+    var title = document.querySelector("h2");
+    title && (title.onclick = selectOutField);
+  }());
+    
+  (function(){
+    var resetOutButtonState = function() {
+      var out = document.querySelector(".out form.add button");
+      if (this.value == "") {
+        out.setAttribute('disabled', 'disabled');
+      } else {
+        out.removeAttribute('disabled');
+      }
+    };
 
-  name.onkeyup = resetOutButtonState
-  name.onkeyup()
-})()
+    var name = document.querySelector(".out form.add input#name");
+
+    name.onkeyup = resetOutButtonState;
+    name.onkeyup();
+  }());
+})();
